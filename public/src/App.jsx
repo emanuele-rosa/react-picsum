9a09ff7a29def2dc02a4e5dcb6d14a4f240e7e74
Larissa Ishikawa <l.ishikawacunha@gmail.com>
2024-11-14 20:14:06 -0300

    chore: connect components within App

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { theme } from './assets/styles/theme';
+import Layout from './components/Layout/Layout';
+import Hero from './components/Hero/Hero';
+import ImageGrid from './components/ImageGrid/ImageGrid';
+import { ImageContextProvider } from './contexts/imageContext';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <ImageContextProvider>
+        <Layout>
+          <Hero />
+          <ImageGrid />
+        </Layout>
+      </ImageContextProvider>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
